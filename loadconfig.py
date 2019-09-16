45d48846bc64d5bdd27d8ee7185acd40d62ff426
CaladBlo <CaladBlogBaal@gmail.com>
2019-09-16 22:51:44 +0100

    Create loadconfig.py
    shouldn't have added this to git ignore in hindsight

---
+++ loadconfig.py
@@ -0,0 +1,15 @@
+import os
+
+__username__ = "Victorique"
+__token__ = os.environ.get("BOT_TOKEN")
+__prefix__ = os.environ.get("BOT_PREFIX")
+__owner_ids__ = os.environ.get("BOT_OWNER_IDS")
+__owner_ids__ = [int(owner_id) for owner_id in __owner_ids__.split(" ")]
+
+credentials = "postgres://postgres:InsertPassWord@LocalHost:5432/InsertDatabase"
+
+__img_flip_username__ = "InsertName"
+__img_flip_password__ = "InsertPassword"
+
+from config.cogs import __cogs__
+from config.presence import __presences__, __presenceTimer__
