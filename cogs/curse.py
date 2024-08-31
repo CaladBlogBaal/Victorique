cb4485bf7f22f8f77bd86a2790ca04099b69aa01
Caladblo <caladblogbaal@gmail.com>
2024-08-31 23:27:27 +0100

    Finished:tm:

--- cogs/curse.py
+++ cogs/curse.py
@@ -4,7 +4,7 @@ import random
 import datetime
 import humanize as h
 
-from collections import Counter, namedtuple
+from collections import namedtuple
 
 import discord
 from discord.ext import commands, tasks
@@ -21,66 +21,94 @@ class Curse(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.emotes = {1: "🥇", 2: "🥈", 3: "🥉"}
-        self.curse = namedtuple("curse", "message curse_time cooldown")
 
-    def get_critical_attack_message_and_hours(self, attacker: int, defender: int, member: discord.Member,
-                                              nickname: str, cooldown: datetime.datetime) -> [str, datetime.datetime]:
+    async def edit_nickname(self, target: discord.Member, name: str):
+        try:
+
+            await target.edit(nick=name)
+
+        except discord.errors.Forbidden:
+            raise commands.BadArgument("An error occurred trying to edit a nickname, probably due role hierarchy")
+
+    def calculate_critical_attack(self, attacker: int, defender: int) -> (str, int):
         if defender == 1 and attacker == 20:
-            message = ":dart: Nat 20 Critical attack against a critical failure! :dart:\n"
-            curse_hours = 168
+            return ":dart: Nat 20 Critical attack against a critical failure! :dart:\n", 168
         elif attacker == 20:
-            message = ":dart: Nat 20 Critical hit! :dart:\n"
-            curse_hours = 24
+            return ":dart: Nat 20 Critical hit! :dart:\n", 24
         elif defender == 1:
-            message = ":x: Critical defending failure! :x:\n"
-            curse_hours = 24
+            return ":x: Critical defending failure! :x:\n", 24
         else:
-            message = ""
-            curse_hours = 2
+            return "", 2
 
-        curse_time = discord.utils.utcnow().replace(tzinfo=None) + datetime.timedelta(hours=curse_hours)
-
-        message = self.format_message(member.mention, nickname, attacker, defender, cooldown,
-                                      starter_message=message, curse_hours=curse_hours)
-
-        return message, curse_time
-
-    async def do_defending_message_and_hours(self, ctx, attacker: int, defender: int, member: discord.Member,
-                                             nickname: str, cooldown: datetime.datetime) -> [str, datetime.timedelta,
-                                                                                             datetime.datetime]:
+    def calculate_defense(self, attacker: int, defender: int,
+                          member_name: str, author_name: str) -> (str, int, int, bool):
         if defender == 20:
             if attacker == 1:
-                cooldown = discord.utils.utcnow().replace(tzinfo=None) + datetime.timedelta(days=7)
-                message = f"{member.nick}...and it ended up being reflected back at {ctx.author.mention}!"
-                curse_hours = 168
-                member = ctx.author
+                return f"{member_name}...and it ended up being reflected back at {author_name}!", 168, 168, True
             else:
-                random_member = await self.get_random_active_member(ctx, member) or ctx.author
-                message = f"{member.nick}...and it ended up hitting {random_member.mention} by mistake!"
-                curse_hours = 2
-                try:
-                    await random_member.edit(nick=nickname)
-                    member = random_member
-                except discord.errors.Forbidden:
-                    return await ctx.send(
-                        "An error occurred trying to edit a nickname, probably due to role hierarchy.")
+                return f"{member_name}...and it ended up hitting someone else by mistake!", 2, 2, False
         elif attacker == 1:
-            cooldown = discord.utils.utcnow().replace(tzinfo=None) + datetime.timedelta(days=1)
-            message = ":x: Critical attacking failure! :x:\n"
-            curse_hours = 24
+            return ":x: Critical attacking failure! :x:\n", 24, 24, False
         else:
-            message = ""
-            curse_hours = 2
+            return "", 2, 2, False
+
+    async def resolve_equal_scenario(self, attacker: int, defender: int, member: discord.Member,
+                                     nickname: str, author: discord.Member) -> (str, datetime.datetime):
+        cooldown = discord.utils.utcnow().replace(tzinfo=None) + datetime.timedelta(hours=2)
+        curse_time = discord.utils.utcnow().replace(tzinfo=None) + datetime.timedelta(hours=1)
+        now = discord.utils.utcnow().replace(tzinfo=None)
+
+        if attacker == defender:
+            duration = h.naturaltime(now - curse_time)
+            message = (
+                ":crossed_swords: Both sides have gotten hurt due to an equal exchange.\n"
+                f":white_check_mark: (Attacker: 1d20 ({attacker}) = {attacker}) vs. "
+                f"(Defender: 1d20 ({defender}) = {defender})\n"
+                f"Cursed {member.mention}'s to `{nickname}` until `{duration}`.\n"
+                f"Cursed {author.mention}'s to `{nickname}` for {duration}.\n"
+                f"Your ability to curse is on cooldown until {h.naturaltime(cooldown)}."
+            )
+
+            await self.edit_nickname(author, nickname)
+            return message, curse_time
+
+    def resolve_attack_scenario(self, attacker: int, defender: int, member: discord.Member,
+                                nickname: str, cooldown: datetime.datetime) -> (str, datetime.datetime):
+
+        critical_message, curse_hours = self.calculate_critical_attack(attacker, defender)
+        curse_time = discord.utils.utcnow().replace(tzinfo=None) + datetime.timedelta(hours=curse_hours)
+        message = self.format_message(member.mention, nickname, attacker,
+                                      defender, cooldown, True, curse_time, starter_message=critical_message)
+        return message, curse_time
+
+    async def resolve_defense_scenario(self, ctx, attacker: int, defender: int, member: discord.Member,
+                                       nickname: str) -> (str, datetime.datetime, datetime.datetime):
+
+        message, curse_hours, cooldown_hours, reflect_back = self.calculate_defense(
+            attacker, defender, member.nick, ctx.author.mention
+        )
+
+        cooldown = discord.utils.utcnow().replace(tzinfo=None) + datetime.timedelta(hours=cooldown_hours)
+
+        if reflect_back:
+            member = ctx.author
+
+        # Handle nickname editing and random member selection
+        if "hitting someone else" in message:
+            random_member = await self.get_random_active_member(ctx, member) or ctx.author
+            message = message.replace("someone else", random_member.mention)
+            await self.edit_nickname(random_member, nickname)
 
         curse_time = discord.utils.utcnow().replace(tzinfo=None) + datetime.timedelta(hours=curse_hours)
 
-        message = self.format_message(member.mention, nickname, attacker, defender, cooldown,
-                                      starter_message=message, curse_hours=curse_hours)
+        message = self.format_message(
+            member.mention, nickname, attacker, defender, cooldown, False, curse_time, starter_message=message
+        )
 
         return message, curse_time, cooldown
 
     async def record_cursed_event(self, ctx: Context, con, target: discord.Member, nickname: str,
-                                      curse_time: datetime.datetime, success: bool) -> None:
+                                  curse_time: datetime.datetime, success: bool) -> None:
         """Record a cursed event in the database."""
         await con.execute("""INSERT INTO cursed_event 
                             (cursed_by_user_id, cursed_user_id, curse_at, curse_success, curse_length) VALUES 
@@ -98,12 +126,8 @@ class Curse(commands.Cog):
     async def update_curse_for_winner(self, ctx: Context, con, target: discord.Member, nickname: str,
                                       curse_time: datetime.datetime):
         """Update curse records for a winning attacker."""
-        try:
 
-            await target.edit(nick=nickname)
-
-        except discord.errors.Forbidden:
-            return await ctx.send("An error occurred trying to edit a nickname, probably due role hierarchy")
+        await self.edit_nickname(target, nickname)
 
         await con.execute("""INSERT INTO cursed_user 
                                     (user_id, curse_at, curse_name, curse_cooldown, curse_ends_at) VALUES 
@@ -139,49 +163,38 @@ class Curse(commands.Cog):
 
         await ctx.send(message)
 
+    def calculate_attack_outcome(self, attacker: int, defender: int) -> namedtuple:
+        attacker_won = attacker > defender
+        cooldown = datetime.timedelta(hours=2)
+
+        if attacker == defender:
+            result_type = "equal"
+        elif attacker_won:
+            result_type = "attacker_won"
+        else:
+            result_type = "defender_won"
+
+        return namedtuple("Outcome", ["result_type", "cooldown"])(result_type, cooldown)
+
     async def attempt_attack(self, ctx, attacker: int, defender: int, member: discord.Member,
                              nickname: str) -> namedtuple:
 
-        cooldown = discord.utils.utcnow().replace(tzinfo=None) + datetime.timedelta(hours=2)
-        curse_time = discord.utils.utcnow().replace(tzinfo=None) + datetime.timedelta(hours=1)
-        now = discord.utils.utcnow().replace(tzinfo=None)
+        outcome = self.calculate_attack_outcome(attacker, defender)
+        cooldown = discord.utils.utcnow().replace(tzinfo=None) + outcome.cooldown
 
-        if attacker == defender:
-            duration = h.naturaltime(now - curse_time)
-            message = (
-                ":crossed_swords: Both sides have gotten hurt due to an equal exchange.\n"
-                f":white_check_mark: (Attacker: 1d20 ({attacker}) = {attacker}) vs. "
-                f"(Defender: 1d20 ({defender}) = {defender})\n"
-                f"Cursed {member.mention}'s to `{nickname}` until `{duration}`.\n"
-                f"Cursed {ctx.author.mention}'s to `{nickname}` for {duration}.\n"
-                f"Your ability to curse is on cooldown until {h.naturaltime(cooldown)}."
-            )
+        if outcome.result_type == "equal":
+            message, curse_time = await self.resolve_equal_scenario(attacker, defender, member, nickname, ctx.author)
 
-            try:
+        elif outcome.result_type == "attacker_won":
+            message, curse_time = self.resolve_attack_scenario(attacker, defender, member, nickname, cooldown)
+        else:  # defender > attacker
+            message, curse_time, cooldown = await self.resolve_defense_scenario(
+                ctx, attacker, defender, member, nickname)
 
-                await ctx.author.edit(nick=nickname)
-
-            except discord.errors.Forbidden:
-                return await ctx.send("An error occurred trying to edit a nickname, probably due role hierarchy.")
-
-        else:
-            attacker_won = attacker > defender
-
-            if attacker_won:
-
-                message, curse_time = self.get_critical_attack_message_and_hours(attacker, defender,
-                                                                                 member, nickname, cooldown)
-            else:  # defender > attacker
-                message, curse_time, cooldown = await self.do_defending_message_and_hours(ctx, attacker, defender,
-                                                                                          member, nickname, cooldown)
-
-        curse = self.curse(message, curse_time, cooldown)
-        return curse
+        return namedtuple("curse", ["message", "curse_time", "cooldown"])(message, curse_time, cooldown)
 
     def format_message(self, member_name: str, nickname: str, attacker: int, defender: int, cooldown: datetime,
-                       starter_message: str = "", attack=True, curse_hours=2) -> str:
-
-        curse_time = discord.utils.utcnow().replace(tzinfo=None) + datetime.timedelta(hours=curse_hours)
+                       attack: bool, curse_time: datetime, starter_message: str = "") -> str:
 
         if attack:
             return self.format_attack(member_name, nickname, attacker, defender, curse_time,
@@ -326,16 +339,28 @@ class Curse(commands.Cog):
 
         await self.get_member_stats(ctx, member)
 
+    def build_embed(self, title: str, description: str, fields: list, author: discord.Member = None) -> discord.Embed:
+        embed = discord.Embed(title=title, description=description, color=self.bot.default_colors())
+        for name, value in fields:
+            embed.add_field(name=name, value=value, inline=False)
+        if author:
+            embed.set_author(name=author.name, icon_url=str(author.avatar.url))
+        return embed
+
+    def calculate_success_rate(self, total_curses: int, successful_curses: int) -> float:
+        return successful_curses / total_curses if total_curses > 0 else 0
+
     async def get_guild_stats(self, ctx):
         query = """
             WITH stats AS (
-                SELECT cursed_by_user_id, curse_length, curse_success, COUNT(curse_success) AS successful_curses,
+                SELECT cursed_by_user_id, COUNT(*) AS total_curses, 
+                        SUM(CASE WHEN curse_success THEN 1 ELSE 0 END) AS successful_curses,
                         RANK() OVER (ORDER BY COUNT(*) DESC) rank
                 FROM cursed_event
                 WHERE curse_at = $1
-                GROUP BY cursed_by_user_id, curse_length, curse_success
+                GROUP BY cursed_by_user_id
                 )
-                SELECT cursed_by_user_id, curse_length, successful_curses as sc, rank
+                SELECT cursed_by_user_id, total_curses, successful_curses as sc, rank
                 FROM stats                      
         """
 
@@ -343,56 +368,62 @@ class Curse(commands.Cog):
 
         if not data:
             return await ctx.send("> No curses have happened yet for this guild.")
+        # data is already ranked by total curses
+        top_cursers = data[:3]
+        top_successful_cursers = sorted(data, key=lambda r: self.calculate_success_rate(r["total_curses"], r["sc"]),
+                                        reverse=True)[:3]
 
-        top_cursers = Counter(r["cursed_by_user_id"] for r in data).most_common(3)
+        total_curses = sum(r["total_curses"] for r in data)
+        total_sc = sum(r["sc"] for r in data)
 
-        description = f"{len(data)} curses, which {len(data) / sum(d['sc'] for d in data):.1%} were successful"
+        description = f"{total_curses} curses, of which {self.calculate_success_rate(total_curses, total_sc):.1%} " \
+                      f"were successful"
 
-        embed = discord.Embed(title="Curse Stats",
-                              color=self.bot.default_colors())
+        top_field = "\n".join(f"{self.emotes[i + 1]}: <@{t[0]}> ({h.intcomma(t[1])} curses)"
+                              for i, t in enumerate(top_cursers))
 
-        embed.description = description
+        success_field = "\n".join(f"{self.emotes[i + 1]}: <@{r['cursed_by_user_id']}> "
+                                  f"({self.calculate_success_rate(r['total_curses'], r['sc']):.1%} times)"
+                                  for i, r in enumerate(top_successful_cursers))
 
-        value = "\n".join(f"{self.emotes[i + 1]}: <@{t[0]}> ({h.intcomma(t[1])} curses)"
-                          for i, t in enumerate(top_cursers))
-        embed.add_field(name="Top curser(s)", value=value, inline=False)
+        fields = [
+            ("Top curser(s)", top_field),
+            ("Most successful curser(s)", success_field)
+        ]
 
-        value = "\n".join(f"{self.emotes[i + 1]}: <@{r['cursed_by_user_id']}> "
-                          f"({len(data) / r['sc']:.1%} times)"
-                          for i, r in enumerate(data[:3]))
-
-        embed.add_field(name="Most successful curser(s)", value=value, inline=False)
+        embed = self.build_embed(title="Curse Stats", description=description, fields=fields)
 
         await ctx.send(embed=embed)
 
     async def get_member_stats(self, ctx, member: discord.Member):
         query = """
-                  WITH stats AS (
-                      SELECT cursed_by_user_id, curse_length, curse_success, COUNT(curse_success) AS successful_curses,
-                              RANK() OVER (ORDER BY COUNT(*) DESC) rank
-                      FROM cursed_event
-                      WHERE curse_at = $1 and cursed_by_user_id = $2
-                      GROUP BY cursed_by_user_id, curse_length, curse_success
-                      )
-                      SELECT cursed_by_user_id, curse_length, successful_curses, rank
-                      FROM stats                      
-              """
+            WITH stats AS (
+                SELECT cursed_by_user_id, COUNT(*) AS total_curses, 
+                        SUM(CASE WHEN curse_success THEN 1 ELSE 0 END) AS successful_curses,
+                        RANK() OVER (ORDER BY COUNT(*) DESC) rank
+                FROM cursed_event
+                WHERE curse_at = $1 and cursed_by_user_id =  $2
+                GROUP BY cursed_by_user_id
+                )
+                SELECT cursed_by_user_id, total_curses, successful_curses as sc, rank
+                FROM stats
+                LIMIT 1                 
+        """
 
-        data = await ctx.db.fetch(query, ctx.guild.id, member.id)
+        data = await ctx.db.fetchrow(query, ctx.guild.id, member.id)
 
         if not data:
             return await ctx.send(f"> {member.name} hasn't cursed anyone yet for this guild.")
 
-        curse_count = len(data)
-        success_rate = data[0]["successful_curses"] / curse_count
+        success_rate = self.calculate_success_rate(data["total_curses"], data["sc"])
 
-        embed = discord.Embed(title="Member Stats",
-                              color=self.bot.default_colors())
+        fields = [
+            ("Curse count", h.intcomma(data["total_curses"])),
+            ("Success Rate", f"{success_rate:.1%}")
+        ]
 
-        embed.add_field(name="Curse count", value=h.intcomma(curse_count), inline=True)
-        embed.add_field(name="Success Rate", value=f"{success_rate:.1%}", inline=True)
+        embed = self.build_embed(title="Member Stats", description="", fields=fields, author=member)
 
-        embed.set_author(name=member.name, icon_url=str(member.avatar.url))
         await ctx.send(embed=embed)
 
 
